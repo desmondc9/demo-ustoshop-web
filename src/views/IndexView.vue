eb937aa25adf6f66f28ce32b83a5b934e75d0271
Desmond Chen <desmondddd@icloud.com>
2023-05-04 22:22:23 +0800

    Changed IndexView.vue

--- src/views/IndexView.vue
+++ src/views/IndexView.vue
@@ -2,9 +2,9 @@
   <v-main class="main">
     <v-container class="d-flex justify-center align-center mt-lg-16 pt-lg-16">
       <v-col>
-        <v-row class="main-title justify-center text-lg-h1 text-h2 font-italic">打爆专家 <br> 强势出击
+        <v-row class="main-title justify-center text-lg-h1 text-h2 font-italic mt-lg-16 pt-lg-16">打爆专家 <br> 强势出击
         </v-row>
-        <v-row class="sub-title-1 justify-center text-lg-h4 text-h5"> - 12.5方不再是梦 -</v-row>
+        <v-row class="sub-title-1 justify-center text-lg-h4 text-h5"> - 12.5方不是梦 -</v-row>
         <v-row class="sub-title-2 justify-center">
           <v-btn @click="redirectToCainiao" class="mt-8" size="x-large" variant="outlined" color="orange-darken-3">
             点击体验
@@ -27,7 +27,7 @@ const redirectToCainiao = () => {
 
 .main {
     height: 95%;
-    background: url("../assets/bg.jpeg") no-repeat center center fixed !important;
+    background: url("../assets/bg2.png") no-repeat center center fixed !important;
     background-size: cover !important;
 }
 
@@ -36,7 +36,7 @@ const redirectToCainiao = () => {
     font-family: "Noto Sans", "Liberation Sans", sans-serif;
     font-weight: bolder;
     color: white;
-    /*text-shadow: -1px 0 rgba(0, 0, 0, 1), 0 1px rgba(0, 0, 0, 1), 1px 0 rgba(0, 0, 0, 1), 0 -1px rgba(0, 0, 0, 1);;*/
+    text-shadow: -1px 0 rgba(0, 0, 0, 0.5), 0 1px rgba(0, 0, 0, 0.5), 1px 0 rgba(0, 0, 0, 0.5), 0 -1px rgba(0, 0, 0, 0.5);
 }
 
 .sub-title-1 {
