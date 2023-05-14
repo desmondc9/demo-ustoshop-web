e1d4f38e9263fb6250fa1bf7be0fde881cf6c6f9
Desmond Chen <desmondddd@icloud.com>
2023-05-14 22:58:04 +0800

    Changed AboutUsView.vue

--- src/views/AboutUsView.vue
+++ src/views/AboutUsView.vue
@@ -4,19 +4,39 @@
 
 <template>
   <v-main class="main">
-    <v-container class="d-flex justify-center align-center fill-height pt-0">
-      <v-col class="fill-height">
-        <v-row class="justify-center">
-          <v-img
-            :height="1500"
-            cover
-            src="./aboutus_1.jpg"></v-img>
+    <v-container class="d-flex justify-center align-center pt-0">
+      <v-col class="d-flex flex-column justify-start">
+
+        <v-row class="d-flex title text-h3 align-self-center mt-5">关于我们</v-row>
+
+        <v-row>
+          <v-divider class="mt-10"></v-divider>
         </v-row>
-        <v-row class="justify-center">
-          <v-img
-            :height="1800"
-            cover
-            src="./aboutus_2.jpeg"></v-img>
+
+        <v-row class="d-flex justify-center align-self-center mt-10">
+          <img class="contact-qrcode" src="aboutus-3.png" alt="contact qrcode" />
+        </v-row>
+
+        <v-row class="d-flex justify-center align-self-center mt-10 text-h5">
+          <p class="d-flex">扫描上方二维码</p>
+        </v-row>
+        <v-row class="d-flex justify-center align-self-center mt-5 text-h5">
+          <p>关注公众号, 即可获得</p>
+        </v-row>
+        <v-row class="d-flex justify-center align-self-center mt-5 text-h5">
+          <p>更多打板优惠, 更多行业资讯</p>
+        </v-row>
+
+        <v-row class="d-flex justify-center align-self-center mt-10">
+          <img class="contact-qrcode" src="aboutus-4.png" alt="contact qrcode" />
+        </v-row>
+
+        <v-row class="d-flex justify-center align-self-center mt-10 text-h5">
+
+        <p class="d-flex">扫描上方二维码</p>
+        </v-row>
+        <v-row class="d-flex justify-center align-self-center mt-5 text-h5">
+          <p>加入客户服务群, 可获得使用指导</p>
         </v-row>
 
       </v-col>
@@ -27,8 +47,15 @@
 </template>
 
 <style scoped>
-.main {
-    height: 95%;
+
+
+.title {
+    color: #f47a00;
+}
+
+.contact-qrcode {
+    height: 300px;
+    width: auto;
 }
 
 
